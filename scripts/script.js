f7397aac87baeed77edee52a8843ec5632e84d9d
Larry McConville <larry.w.mcconville@gmail.com>
2020-10-15 14:00:57 -0500

    added card-deck and cards for 5-Day Forecast

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@
 //var queryURL = baseURL + locationURL + apiKey;
 var cityToSearch = "";
 var loadLastCity = true;
+var apiKey = "&APPID=f97d09aad6f01e913a987bee4e1619fb";
 
 cityHistory = [];
 
@@ -33,16 +34,16 @@ function searchCityWeather() {
   var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
   var locationURL = cityToSearch;
   var locationUnits = "&units=imperial";
-  var apiKey = "&APPID=f97d09aad6f01e913a987bee4e1619fb";
+  //var apiKey = "&APPID=f97d09aad6f01e913a987bee4e1619fb";
   var queryURL = baseURL + locationURL + locationUnits + apiKey;
 
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    //console.log(response);
     var cityName = response.name;
-    var cityDate = response.dt;
+    var cityDate = moment.unix(response.dt).format("YYYY-MM-DD");
     var cityIcon = response.weather[0].icon;
     var cityIconURL = "http://openweathermap.org/img/w/" + cityIcon + ".png";
     var cityTemp = response.main.temp;
@@ -59,24 +60,54 @@ function searchCityWeather() {
 }
 
 function UVIndex(lat, lon) {
+  var cityUV = "";
+
   queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?&appid=f97d09aad6f01e913a987bee4e1619fb&lat=" + lat + "&lon=" + lon;
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response[0].value);
-    return response[0].value;
+    cityUV = response[0].value;
   });
+  return cityUV;
 }
 
 function searchCityForecast() {
-  renderCityForecast();
+  var baseURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+  var locationURL = cityToSearch;
+  var locationUnits = "&units=imperial";
+  //var apiKey = "&APPID=f97d09aad6f01e913a987bee4e1619fb";
+  var queryURL = baseURL + locationURL + locationUnits + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //console.log(response);
+    //loop through response 5 times beginning with current day +1
+    for (var i = 0; i < response.list.length; i++) {
+      if (response.list[i].dt_txt[12] === "2") {
+        var forecastDate = moment.unix(response.list[i].dt).format("YYYY-MM-DD");
+        var forecastTemp = response.list[i].main.temp;
+        var forecastHumidity = response.list[i].main.humidity;
+        var forecastIcon = response.list[i].weather[0].icon;
+        var forecastIconURL = "http://openweathermap.org/img/w/" + forecastIcon + ".png";
+        //console.log(response.list[i].dt_txt);
+        //console.log(moment.unix(response.list[i].dt).format("YYYY-MM-DD")); //.format(forecastDate, "YYYY-MM-DD"));
+        //console.log(forecastTemp);
+        //console.log(forecastHumidity);
+
+        renderCityForecast(forecastDate, forecastTemp, forecastHumidity, forecastIconURL);
+      }
+    }
+  });
 }
 
 function renderCityWeather(cityName, cityDate, cityTemp, cityHumidity, cityWindSpeed, cityUV, cityIconURL) {
   //TODO:need to do moment date to convert city date
   //TODO:need to lookup UV indicator
-  var cardDiv = $("<div>").attr("class", "card city-card");
+  var card = $("<div>").attr("class", "card city-card");
   var cardBody = $("<div>").attr("class", "card-body city-card-body");
   var cardCity = $("<h3>")
     .attr("class", "card-title city-card-title")
@@ -94,10 +125,24 @@ function renderCityWeather(cityName, cityDate, cityTemp, cityHumidity, cityWindS
   var cardCityUV = $("<p>")
     .attr("class", "card-text city-card-text")
     .text("UV Index: " + cityUV);
-  $("#city-summary").append(cardDiv, cardBody, cardCity, cardTemp, cardHumidity, cardWindSpeed, cardCityUV);
+  $("#city-summary").append(card, cardBody, cardCity, cardTemp, cardHumidity, cardWindSpeed, cardCityUV);
 }
 
-function renderCityForecast() {}
+function renderCityForecast(forecastDate, forecastTemp, forecastHumidity, forecastIconURL) {
+  //var cardDeck = $("<div>").attr("class", "card-deck forecast-card-deck");
+  var card = $("<div>").attr("class", "card bg-primary forecast-card");
+  var cardBody = $("<div>").attr("class", "card-body forecast-card-body");
+  var cardDate = $("<h6>").attr("class", "card-title forecast-card-title").text(forecastDate);
+  var cardIcon = $("<img>").attr("class", "icon").attr("src", forecastIconURL).attr("alt", "Weather Icon");
+  var cardTemp = $("<p>")
+    .attr("class", "card-text forecast-card-text")
+    .text("Temp: " + forecastTemp + " °F");
+  var cardHumidity = $("<p>")
+    .attr("class", "card-text forecast-card-text")
+    .text("Humidity: " + forecastHumidity + " %");
+  $(card).append(cardBody, cardDate, cardIcon, cardTemp, cardHumidity);
+  $("#forecast-card-deck").append(card);
+}
 
 function renderCityHistory() {
   $("#search-list").empty();
@@ -110,10 +155,8 @@ function renderCityHistory() {
 
 function getCitiesFromStorage() {
   storedCityHistory = JSON.parse(localStorage.getItem("cityhistory"));
-  //console.log(storedCityHistory);
   if (storedCityHistory !== null) {
     cityHistory = storedCityHistory;
-    //console.log(cityHistory);
     renderCityHistory();
   }
 }
@@ -145,6 +188,4 @@ $("#search-button").click(function (event) {
   saveCityToStorage();
   searchCityWeather();
   searchCityForecast();
-
-  //console.log(cityToSearch);
 });
